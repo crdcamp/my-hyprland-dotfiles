8cca75d92d460513b14e95c607bc329e0cbf771b
Christian <crdcamp@gmail.com>
2026-04-17 01:25:50 +0000

    More notes on next steps

--- install.sh
+++ install.sh
@@ -22,7 +22,12 @@ stow --dir=~/.dotfiles --target=~/
 # Change to zsh from bash
 
 # Setup xdg-desktop-portal-shana before continuing (this doesn't apply to scripting)
+# DON'T FORGET TO ADD THIS TO GIT
+
+# Configure Nautilus to use zed
 
 # Setup synced clock (I forget how to do this)
 
 # Create symlinks to xdg for git tracking
+
+# Setup timeshift
