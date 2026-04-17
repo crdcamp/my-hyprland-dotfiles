f3cd2ef6accb80c5a98492589de0732886d223dc
Christian <crdcamp@gmail.com>
2026-04-17 01:16:12 +0000

    Next steps

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # Ensure we're in the home folder
 cd ~/
 
+# Install all dependencies
+#...............
+
 # Clone Hyprland configuration files and install them
 git clone https://github.com/crdcamp/my-hyprland-dotfiles.git
 mkdir .dotfiles
@@ -8,3 +11,11 @@ cp -r ~/my-hyprland-dotfiles ~/.dotfiles
 
 # Setup gnu stow
 stow --dir=~/.dotfiles --target=~/
+
+# Next steps:
+
+# Setup synced clock (I forget how to do this)
+
+# Setup xdg-desktop-portal-shana before continuing (this doesn't apply to scripting)
+
+# Create symlinks to xdg for git tracking
