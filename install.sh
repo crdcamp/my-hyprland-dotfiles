4acf38822d0cb9ae5cf2339109a55f3c811aa7b9
Christian <crdcamp@gmail.com>
2026-04-17 02:28:31 +0000

    xdg-desktop-portal setup

--- install.sh
+++ install.sh
@@ -1,40 +1,55 @@
-# THIS INSTALL SCRIPT ASSUMES A FRESH MINIMAL ARCH INSTALL
+#!/usr/bin/env bash
+# NEED TO FIGURE OUT HOW TO ACCEPT ALL PROMPTS DURING INSTALL
 
 # Ensure we're in the home folder
 cd ~/
 
-# Install git
+# Install git and yay
 pacman -S git
-
-# Install yay
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
+# Homefolder... again
 cd ~/
-
-
 
 # CREATE FRESH PACKAGES.TXT FILE BEFORE DOING THIS
 # Install all dependencies by reading packages.txt
-#...............
 
 # Delete auto generated hyprland configs
+rm -rf ~/.config
 
 # Change to zsh from bash
 
-# Clone Hyprland configuration files and install them
+# Clone Hyprland configuration files and copy them
 git clone https://github.com/crdcamp/my-hyprland-dotfiles.git
 mkdir .dotfiles
 cp -r ~/my-hyprland-dotfiles ~/.dotfiles
 
-# Setup gnu stow
+# GNU Stow
 stow --dir=~/.dotfiles --target=~/
 
-# Next steps:
+# XDG-DESKTOP-PORTAL SETUP
+# Create `xdg-desktop-portal` and `xdg-desktop-portal-shana` directories
+mkdir -p ~/.config/{xdg-desktop-portal-shana,xdg-desktop-portal}
+touch ~/.config/xdg-desktop-portal-shana/config.toml
+touch ~/.config/xdg-desktop-portal/hyprland-portals.conf
 
-# Setup xdg-desktop-portal-shana before continuing (this doesn't apply to scripting)
-# DON'T FORGET TO ADD THIS TO GIT
+# `xdg-desktop-portal-shana` config
+cat > ~/.config/xdg-desktop-portal-shana/config.toml << 'EOF'
+open_file = "Gnome"
+save_file = "Gnome"
 
-# Configure Nautilus to use zed
+[tips]
+open_file_when_folder = "Gnome"
+EOF
+
+# `xdg-desktop-portal` config
+cat > ~/.config/xdg-desktop-portal/hyprland-portals.conf << 'EOF'
+[preferred]
+default=hyprland;gtk
+org.freedesktop.impl.portal.FileChooser=shana
+EOF
+
+# Configure Nautilus to use Zed Editor
 
 # Setup synced clock (I forget how to do this)
 
