441201d6d242dfe51b08490ab193c0eb80e51293
Christian <crdcamp@gmail.com>
2026-04-17 01:58:53 +0000

    Git and yay installs

--- install.sh
+++ install.sh
@@ -3,11 +3,23 @@
 # Ensure we're in the home folder
 cd ~/
 
+# Install git
+pacman -S git
+
+# Install yay
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+cd ~/
+
+
+
 # CREATE FRESH PACKAGES.TXT FILE BEFORE DOING THIS
 # Install all dependencies by reading packages.txt
 #...............
 
 # Delete auto generated hyprland configs
+
+# Change to zsh from bash
 
 # Clone Hyprland configuration files and install them
 git clone https://github.com/crdcamp/my-hyprland-dotfiles.git
@@ -18,8 +30,6 @@ cp -r ~/my-hyprland-dotfiles ~/.dotfiles
 stow --dir=~/.dotfiles --target=~/
 
 # Next steps:
-
-# Change to zsh from bash
 
 # Setup xdg-desktop-portal-shana before continuing (this doesn't apply to scripting)
 # DON'T FORGET TO ADD THIS TO GIT
