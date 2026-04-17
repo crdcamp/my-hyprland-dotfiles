47bffbe9191c817539657ae5e2658c401ffc8669
Christian <crdcamp@gmail.com>
2026-04-17 01:11:39 +0000

    Notes

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-cd ~
+# Ensure we're in the home folder
+cd ~/
 
+# Clone Hyprland configuration files and install them
 git clone https://github.com/crdcamp/my-hyprland-dotfiles.git
 mkdir .dotfiles
 cp -r ~/my-hyprland-dotfiles ~/.dotfiles
 
+# Setup gnu stow
 stow --dir=~/.dotfiles --target=~/
