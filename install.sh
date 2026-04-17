15325edf7e7991762aa5ba6b5eeb484ca3faaa4a
Christian <crdcamp@gmail.com>
2026-04-17 01:10:23 +0000

    Reinstated copy command

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ cd ~
 
 git clone https://github.com/crdcamp/my-hyprland-dotfiles.git
 mkdir .dotfiles
-mv -r ~/my-hyprland-dotfiles ~/.dotfiles
+cp -r ~/my-hyprland-dotfiles ~/.dotfiles
 
 stow --dir=~/.dotfiles --target=~/
