12769891be01855ffb0ab5a1afaa5010cc583b82
Christian <crdcamp@gmail.com>
2026-04-17 02:37:13 +0000

    Bunch of lil things

--- install.sh
+++ install.sh
@@ -4,22 +4,30 @@
 # Ensure we're in the home folder
 cd ~/
 
-# Install git and yay
-pacman -S git
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+# Install git
+sudo pacman -S git
 
-# Homefolder... again
+# Install yay
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Homefolder... again (you never know)
 cd ~/
+
+# Create quick access folders for Nautilus
+mkdir -p ~/{Code,Documents,Notes}
 
 # CREATE FRESH PACKAGES.TXT FILE BEFORE DOING THIS
 # Install all dependencies by reading packages.txt
 
-# Delete auto generated hyprland configs
+# Delete auto generated configs
 rm -rf ~/.config
 
 # Change to zsh from bash
 
-# Clone Hyprland configuration files and copy them
+# Clone Hyprland configuration files and copy them (we'll stick with manual deletion for now)
 git clone https://github.com/crdcamp/my-hyprland-dotfiles.git
 mkdir .dotfiles
 cp -r ~/my-hyprland-dotfiles ~/.dotfiles
@@ -27,13 +35,12 @@ cp -r ~/my-hyprland-dotfiles ~/.dotfiles
 # GNU Stow
 stow --dir=~/.dotfiles --target=~/
 
-# XDG-DESKTOP-PORTAL SETUP
 # Create `xdg-desktop-portal` and `xdg-desktop-portal-shana` directories
 mkdir -p ~/.config/{xdg-desktop-portal-shana,xdg-desktop-portal}
 touch ~/.config/xdg-desktop-portal-shana/config.toml
 touch ~/.config/xdg-desktop-portal/hyprland-portals.conf
 
-# `xdg-desktop-portal-shana` config
+# Insert `xdg-desktop-portal-shana` config
 cat > ~/.config/xdg-desktop-portal-shana/config.toml << 'EOF'
 open_file = "Gnome"
 save_file = "Gnome"
@@ -42,7 +49,7 @@ save_file = "Gnome"
 open_file_when_folder = "Gnome"
 EOF
 
-# `xdg-desktop-portal` config
+# Insert `xdg-desktop-portal` config
 cat > ~/.config/xdg-desktop-portal/hyprland-portals.conf << 'EOF'
 [preferred]
 default=hyprland;gtk
