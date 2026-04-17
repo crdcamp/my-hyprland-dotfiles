b6570faa0708bd961b3a63307233081e185300a7
Christian <crdcamp@gmail.com>
2026-04-17 01:20:09 +0000

    Notes for next steps

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
+# THIS INSTALL SCRIPT ASSUMES A FRESH MINIMAL ARCH INSTALL
+
 # Ensure we're in the home folder
 cd ~/
 
-# Install all dependencies
+# CREATE FRESH PACKAGES.TXT FILE BEFORE DOING THIS
+# Install all dependencies by reading packages.txt
 #...............
+
+# Delete auto generated hyprland configs
 
 # Clone Hyprland configuration files and install them
 git clone https://github.com/crdcamp/my-hyprland-dotfiles.git
@@ -14,8 +19,10 @@ stow --dir=~/.dotfiles --target=~/
 
 # Next steps:
 
-# Setup synced clock (I forget how to do this)
+# Change to zsh from bash
 
 # Setup xdg-desktop-portal-shana before continuing (this doesn't apply to scripting)
+
+# Setup synced clock (I forget how to do this)
 
 # Create symlinks to xdg for git tracking
