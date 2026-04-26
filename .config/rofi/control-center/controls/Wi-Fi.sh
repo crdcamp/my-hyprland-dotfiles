781ff96f588ab2c76d996342f1cbf3aad34499fc
Christian <crdcamp@gmail.com>
2026-04-26 03:47:32 +0000

    Cleaned get-networks results

--- .config/rofi/control-center/controls/Wi-Fi.sh
+++ .config/rofi/control-center/controls/Wi-Fi.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-$device_list=iwctl && device list
+# ADD A CHECK TO SEE IF WIFI IS RUNNING... or however you say that
+# Make sure you're checking the device list correctly
+
+iwctl device list
+
+# Get some clean results for the networks
+iwctl station wlan0 get-networks | tail -n +5 | head -n -1
+
+# We'll have to pipe the above results into rofi now
+
+# DON'T FORGET TO STOP STATION SCAN
 $station=
 
 # Exit iwctl when done
