088c293395f9288b3ed07a7be98f34c5f71d311d
Christian <crdcamp@gmail.com>
2026-04-26 01:11:58 +0000

    Control center showing correctly now

--- .config/rofi/control-center/open-controls.sh
+++ .config/rofi/control-center/open-controls.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 control_center_dir="${HOME}/.dotfiles/.config/rofi/control-center/controls"
+cd control_center_dir
 rofi -show filebrowser -filebrowser-dir "$control_center_dir"
