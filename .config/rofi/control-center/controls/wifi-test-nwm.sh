1328529db38e9ffbbaf5712fadda77f82a3d68e8
Christian <crdcamp@gmail.com>
2026-04-26 18:30:11 +0000

    Start NM function complete

--- .config/rofi/control-center/controls/wifi-test-nwm.sh
+++ .config/rofi/control-center/controls/wifi-test-nwm.sh
@@ -2,15 +2,20 @@
 
 # Code borrowed from: https://github.com/P3rf/rofi-network-manager/blob/master/src/ronema
 
-# Check if network manager is running
-nm_status() {
+# Check if network manager is running and start it if it's not
+start_nm() {
     if [ "$(systemctl is-active NetworkManager.service)" != "inactive" ];
+        then
         echo 'NetworkManager.service is already running'
         return
     fi
     sudo systemctl start NetworkManager.service
     sudo systemctl enable NetworkManager.service
+    echo 'Started/Enables NetworkManager.service'
 }
+
+# Function to shut down NetworkManager
+
 
 # Function for wireless interface
 
