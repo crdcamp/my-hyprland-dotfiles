b68954993478b0d546c20be9dfb4be1ec1eb0963
Christian <crdcamp@gmail.com>
2026-04-26 01:44:17 +0000

    Took some things from GitHub. Need to get them working

--- .config/rofi/control-center/open-controls.sh
+++ .config/rofi/control-center/open-controls.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 control_center_dir="${HOME}/.dotfiles/.config/rofi/control-center/controls"
-cd control_center_dir
-rofi -show filebrowser -filebrowser-dir "$control_center_dir"
+
+# Present scripts to user, capture selection
+selected=$(ls "$control_center_dir" | rofi -dmenu -p "Control Center")
+
+# Execute the selected script if one was chosen
+if [[ -n "$selected" ]]; then
+    bash "${control_center_dir}/${selected}"
+fi
