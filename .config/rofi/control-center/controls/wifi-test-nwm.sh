ccbe2b90a4e52b8bb9465c4dd5b89dc7956132d5
Christian <crdcamp@gmail.com>
2026-04-26 18:40:27 +0000

    Wired and wireless interfaces

--- .config/rofi/control-center/controls/wifi-test-nwm.sh
+++ .config/rofi/control-center/controls/wifi-test-nwm.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-
-# Code borrowed from: https://github.com/P3rf/rofi-network-manager/blob/master/src/ronema
+WIRELESS_INTERFACES=($(nmcli device | awk '$2=="wifi" {print $1}'))
+WIRED_INTERFACES=($(nmcli device | awk '$2=="ethernet" {print $1}'))
+# Some code was borrowed from: https://github.com/P3rf/rofi-network-manager/blob/master/src/ronema
 
 # Function to check if network manager is running and start it if it's not
 start_nm() {
-    if [ "$(systemctl is-active NetworkManager.service)" != "inactive" ];
+    if [ "$(systemctl is-active NetworkManager.service)" = "active" ];
         then
         echo 'NetworkManager.service is already running'
         return
