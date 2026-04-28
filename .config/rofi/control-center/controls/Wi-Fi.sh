9a05f0a9771f8b3b9c9250c52ce43551011b9722
Christian <crdcamp@gmail.com>
2026-04-28 01:25:05 +0000

    Notes for next steps

--- .config/rofi/control-center/controls/Wi-Fi.sh
+++ .config/rofi/control-center/controls/Wi-Fi.sh
@@ -9,6 +9,26 @@ check_nm_status() {
     status="$(systemctl is-active NetworkManager.service)"
 }
 
+# Check wireless connection
+check_nm_wireless_state() {
+    WIFI_CON_STATE=$(nmcli device status | grep "^${WIRELESS_INTERFACES[WLAN_INT]}." | awk '{print $3}')
+
+    # Adjust this to get rid of the CONNECTION and (externally) fields
+    ACTIVE_SSID=$(nmcli device status | grep "^${WIRELESS_INTERFACES[WLAN_INT]}." | awk '{print $4}')
+}
+
+# Check ethernet connection
+check_nm_ethernet_state() {
+    ETHERNET_CON_STATE=$(nmcli device status | grep "ethernet" | head -1 | awk '{print $3}')
+    if [[ "$ETHERNET_CON_STATE" != "" ]]; then
+        ETHERNET_CON_STATE="active"
+        return
+    fi
+    ETHERNET_CON_STATE="inactive"
+}
+
+#active_network()
+
 # Toggle Wi-Fi on/off
 toggle_network() {
     check_nm_status > /dev/null # Mute `check_nm_status` output
@@ -22,23 +42,6 @@ toggle_network() {
     fi
 }
 
-# Check wireless connection
-check_nm_wireless_state() {
-    WIFI_CON_STATE=$(nmcli device status | grep "^${WIRELESS_INTERFACES[WLAN_INT]}." | awk '{print $3}')
-    ACTIVE_SSID=$(nmcli device status | grep "^${WIRELESS_INTERFACES[WLAN_INT]}." | awk '{print $4}')
-
-}
-
-# Check ethernet connection
-check_nm_ethernet_state() {
-    ETHERNET_CON_STATE=$(nmcli device status | grep "ethernet" | head -1 | awk '{print $3}')
-    if [[ "$ETHERNET_CON_STATE" != "" ]]; then
-        ETHERNET_CON_STATE="active"
-        return
-    fi
-    ETHERNET_CON_STATE="inactive"
-}
-
 list_wifi_networks() {
     # Extract the SSID and SIGNAL fields and remove headers
     WIFI_NETWORKS=$(nmcli -f SSID,SIGNAL device wifi | tail -n +2 | sort -nr -k2) # Pretty sure it already sorts by signal, but I'll sort just in case
@@ -46,4 +49,24 @@ list_wifi_networks() {
 }
 # Don't forget a "refresh" option for displaying wi-fi (We'll do this much later)
 
-list_wifi_networks
+: '
+WHAT WE HAVE SO FAR:
+* Check network status (for NM in general)
+* Check wifi state
+* Check ethernet state
+* Toggle network
+* A list of wifi networks
+
+SO, WHAT ELSE DO WE NEED?
+* Currently connected network
+* Connect to network option
+
+BUT, IN THE MEANTIME, LETS JUST OUTPUT WHAT WE
+CURRENTLY HAVE INTO A ROFI MENU
+
+We want it to be a series of options, not a bunch of info overwhelming the user:
+* Currently active network (could be displayed in the search tab)
+* Wi-Fi toggle on/off
+* Option to open a list of available networks (need to adjust network list function to not include the current network)
+* Option to switch to ethernet (this will be annoying to test witht the current laptop. Not a huge priority)
+'
