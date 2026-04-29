33082b22941af03e7f7a07c1e0c7c4f7691b96e8
Christian <crdcamp@gmail.com>
2026-04-29 21:44:14 +0000

    Beginning entry menu

--- .config/rofi/control-center.sh
+++ .config/rofi/control-center.sh
@@ -1,32 +1,6 @@
 #!/usr/bin/env bash
-source "$HOME/.dotfiles/.config/rofi/control-center-menus/network.sh"
-# rofi -show controls -modi "controls:$HOME/.dotfiles/.config/rofi/control-center.sh"
 
-show_root_page() {
-    echo -en "\0data\x1froot\n"
-    echo -en "\0prompt\x1fControl Center\n"
-    echo "Network"
-    echo "Bluetooth"
-    echo "Audio"
-    echo "System"
-}
-
-PAGE="${ROFI_DATA:-root}"
-
-if [[ "$ROFI_RETV" -eq 0 ]]; then
-    show_root_page
-
-elif [[ "$ROFI_RETV" -eq 1 ]]; then
-    case "$PAGE" in
-        "root")
-            case "$1" in
-                "Network")
-                    show_network_page
-                    ;;
-            esac
-            ;;
-        "network")
-            handle_network_selection "$1"
-            ;;
-    esac
+# Root menu
+if [ "$ROFI_RETV" = "0" ]; then
+    echo -en "\0prompt\x1fControl Center\nNetwork\nBluetooth\nAudio\nSystem"
 fi
