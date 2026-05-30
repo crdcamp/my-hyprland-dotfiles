610761f470950b70193b8eeb9911f08309430b5d
Christian <crdcamp@gmail.com>
2026-05-30 18:20:27 +0000

    Idk. Commit messages really don't matter for this repository

--- .config/rofi/control-center.sh
+++ .config/rofi/control-center.sh
@@ -3,4 +3,20 @@ show_control_center() {
     echo -en "\0prompt\x1fControl Center\nNetwork\nBluetooth\nAudio\nSystem"
 }
 
-show_control_center
+case "$1" in
+    "Network")
+        impala
+        ;;
+    "Bluetooth")
+        coproc ( blueman-manager > /dev/null 2>&1 )
+        ;;
+    "Audio")
+        coproc ( pavucontrol > /dev/null 2>&1 )
+        ;;
+    "System")
+        coproc ( gnome-system-monitor > /dev/null 2>&1 )
+        ;;
+    *)
+        show_control_center
+        ;;
+esac
