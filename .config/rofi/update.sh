59872bf0cc340406247eee235ae082519d1ea11e
Christian <crdcamp@gmail.com>
2026-05-30 21:12:05 +0000

    Minor changes to update.sh

--- .config/rofi/update.sh
+++ .config/rofi/update.sh
@@ -3,5 +3,6 @@
 # Need to Add notification for successful update
 sudo timeshift --create --comments "Before system update"
 yay -Syu
-pacman -Qq > ~/.dotfiles/packages.txt && echo "Your package list has been updated"
-sleep 5
+pacman -Qq > ~/.dotfiles/packages.txt && echo "Your package list has been updated. Goodbye."
+sleep 4
+exit
