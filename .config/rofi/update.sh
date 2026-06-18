dc039fc19c1989ff6451ce283e3b4cfa3f213b43
Christian <crdcamp@gmail.com>
2026-06-18 16:41:10 +0000

    Print statement changes

--- .config/rofi/update.sh
+++ .config/rofi/update.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # Need to Add notification for successful update
+echo "Enter your password to update:"
 sudo timeshift --create --comments "Before system update"
 yay -Syu
-pacman -Qq > ~/.dotfiles/packages.txt && echo "Your package list has been updated. Goodbye."
-sleep 4
+pacman -Qq > ~/.dotfiles/packages.txt && echo -e "\nYour package list has been updated. You may want to restart your device.\nThis window will close in 5 seconds..."
+sleep 5
 exit
