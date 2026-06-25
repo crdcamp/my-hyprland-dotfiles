8e52ac6cba82c002a64d5cf9637fef4db293ec9a
Christian <crdcamp@gmail.com>
2026-06-25 19:56:42 +0000

    Variables done

--- .config/hypr/hyprLua/hyprland.lua
+++ .config/hypr/hyprLua/hyprland.lua
@@ -1,5 +1,5 @@
 require("monitors")
-require("variables")
+local vars = require("variables")
 require("lookAndFeel")
 require("keyBindings")
 require("windowRules")
@@ -8,11 +8,41 @@ require("windowRules")
 --- AUTOSTART--
 ---------------
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
--- Or execute your favorite apps at launch like this:
---
 hl.on("hyprland.start", function()
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
     hl.exec_cmd("waybar & hyprpaper & swaync")
     hl.exec_cmd("wl-paste --type text --watch cliphist store")
     hl.exec_cmd("wl-paste --type image --watch cliphist store")
 end)
+
+-----------------------------
+--- ENVIRONMENT VARIABLES ---
+-----------------------------
+--https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
+-- General
+hl.env("XCURSOR_SIZE", "20")
+h1.env("HYPRCURSOR_SIZE", "20")
+hl.env("CLUTTER_BACKEND", "wayland")
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+
+[[Clutter package already has Wayland enabled,
+this variable will force Clutter applications to
+try and use the Wayland backend ]]
+--hl.env("CLUTTER_BACKEND", "wayland")
+
+-- XDG Specifications
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+
+-- Qt variables
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+
+---------
+--INPUT--
+---------
+hl.config({
+    input
+})
--- .config/hypr/hyprLua/variables.lua
+++ .config/hypr/hyprLua/variables.lua
@@ -1,17 +1,20 @@
-return {
+local variables = {
     essentialTools = {
         terminal = "kitty",
         fileManager = "yazi",
         browser = "brave",
         ide = "zeditor",
-},rofi = {
+    },
+    rofi = {
         searchApps = "rofi -show drun",
         searchWindows = "rofi -show window",
         selectWallpaper = "~/.dotfiles/.config/rofi/select-wallpaper.sh",
         controlCenter = [[rofi -show control-center -modi "control-center:~/.dotfiles/.config/rofi/control-center.sh"]],
-        }
-},entertainment = {
+    },
+    entertainment = {
         music = "brave https://open.spotify.com/",
         games = "steam"
-    }
+    },
 }
+
+return variables
