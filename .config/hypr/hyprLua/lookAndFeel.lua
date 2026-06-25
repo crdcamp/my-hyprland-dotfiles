70899ebae176cdcf96994adb53bb4e869bf324ff
Christian <crdcamp@gmail.com>
2026-06-25 22:37:00 +0000

    Layer rules for blur

--- .config/hypr/hyprLua/lookAndFeel.lua
+++ .config/hypr/hyprLua/lookAndFeel.lua
@@ -3,10 +3,29 @@
 ---------------------
 require("colors")
 
--- Waybar Blur
+-- Blur Layer Rules
 hl.layer_rule({
     name = "waybar-blur",
     match = { namespace = waybar },
     blur = true,
     ignore_alpha = 0,
+})
+hl.layer_rule({
+    name = "rofi-blur",
+    match = { namespace = rofi },
+    blur = true,
+    ignore_alpha = 0,
+})
+
+hl.config({
+    general = {
+        gaps_in = 2,
+        gaps_out = 3,
+
+        border_size = 2,
+
+        col = {
+            active_border = { colors = {} }
+        }
+    }
 })
