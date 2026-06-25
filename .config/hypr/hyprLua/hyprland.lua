84d5a15146d36f1505a0de06e55e7b7cb73d4ee4
Christian <crdcamp@gmail.com>
2026-06-25 18:31:04 +0000

    Start up processes

--- .config/hypr/hyprLua/hyprland.lua
+++ .config/hypr/hyprLua/hyprland.lua
@@ -3,3 +3,16 @@ require("variables")
 require("lookAndFeel")
 require("keyBindings")
 require("windowRules")
+
+---------------
+--- AUTOSTART--
+---------------
+-- Autostart necessary processes (like notifications daemons, status bars, etc.)
+-- Or execute your favorite apps at launch like this:
+--
+hl.on("hyprland.start", function()
+    hl.exec_cmd("systemctl --user start hyprpolkitagent")
+    hl.exec_cmd("waybar & hyprpaper & swaync")
+    hl.exec_cmd("wl-paste --type text --watch cliphist store")
+    hl.exec_cmd("wl-paste --type image --watch cliphist store")
+end)
