351bcc78c392bf21b2a21d6f3e2d32281846cc40
Christian <crdcamp@gmail.com>
2026-06-25 19:42:24 +0000

    Working on it

--- .config/hypr/hyprLua/variables.lua
+++ .config/hypr/hyprLua/variables.lua
@@ -1,18 +1,17 @@
-hl.config({
+return {
     essentialTools = {
         terminal = "kitty",
         fileManager = "yazi",
         browser = "brave",
         ide = "zeditor",
-    },
-    rofi = {
+},rofi = {
         searchApps = "rofi -show drun",
         searchWindows = "rofi -show window",
         selectWallpaper = "~/.dotfiles/.config/rofi/select-wallpaper.sh",
         controlCenter = [[rofi -show control-center -modi "control-center:~/.dotfiles/.config/rofi/control-center.sh"]],
-    },
-    entertainment = {
+        }
+},entertainment = {
         music = "brave https://open.spotify.com/",
         games = "steam"
     }
-})
+}
