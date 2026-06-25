37be5a96bef36a08fe1bdf1a68deea1d2f59f921
Christian <crdcamp@gmail.com>
2026-06-25 18:27:29 +0000

    Temporary vairables created

---
+++ .config/hypr/hyprLua/variables.lua
@@ -0,0 +1,15 @@
+terminal = kitty
+fileManager = yazi
+
+browser = "brave"
+IDE = "zeditor"
+
+-- rofi
+searchApps = "rofi -show drun"
+searchWindows = "rofi -show window"
+selectWallpaper = "~/.dotfiles/.config/rofi/select-wallpaper.sh"
+
+openControlCenter = [[rofi -show control-center -modi "control-center:~/.dotfiles/.config/rofi/control-center.sh"]]
+
+music = "brave https://open.spotify.com/"
+steam = "steam"
